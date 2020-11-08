57ac3d6993a9d8802c1d5cb07c18266344c1823a
Gino <gino.jameskutty@gmail.com>
2020-11-08 14:04:54 +0530

    passing child properties

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,8 +4,8 @@ import Greet from './components/Greet'
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce"></Greet>
-      <Greet name="Clark"></Greet>
+      <Greet name="Bruce"><p>child para</p></Greet>
+      <Greet name="Clark"><button>child button</button></Greet>
       <Greet name="Lee"></Greet>
     </div>
   );
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,6 +1,8 @@
 import React from 'react'
 const Greet = props => {
     console.log(props);
-    return <h1>Hello vishwas {props.name}</h1>
+    return (
+        <div><h1>Hello vishwas {props.name}</h1>{props.children}</div>
+    )
 }
 export default Greet;
