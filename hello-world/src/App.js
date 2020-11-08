df49f0928a318a783959998554408bf05318505b
Gino <gino.jameskutty@gmail.com>
2020-11-08 11:21:51 +0530

    a class extending component class of react

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import { Greet } from './components/Greet'
+import Welcome from './components/Welcome'
 function App() {
   return (
     <div className="App">
       <Greet></Greet>
+      <Welcome></Welcome>
     </div>
   );
 }
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+class Welcome extends Component {
+    render() {
+        return <h1>Class Component</h1>
+    }
+}
+export default Welcome;
