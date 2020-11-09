0fb18a919a31b12927cbb1d15bab08c54de084b6
Gino <gino.jameskutty@gmail.com>
2020-11-09 08:46:07 +0530

    destructuring props in body

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,5 +1,6 @@
 import React from 'react'
-const Greet = ({ name, heroName }) => {
+const Greet = props => {
+    const { name, heroName } = props
     return (
         <div><h1>Hello  {name}</h1>{heroName}</div>
     )
