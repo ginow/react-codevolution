bcd8152ea7adecee86a6d752c54ad97ebeefd1bb
Gino <gino.jameskutty@gmail.com>
2020-11-21 22:41:22 +0530

    List rendering: Using index as key

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,15 +1,13 @@
 import React from 'react'
-import Person from './Person'
 
 function NameList() {
     const names= ['Bruce','Clark','Diana', 'Bruce']
-    const nameList = names.map(name=><h2 key={name}>{name}</h2>)
+    const nameList = names.map((name, index)=><h2 key={index}>{index}{name}</h2>)
     return (
         <div>
             {nameList}
         </div>
     )
 }
-
 
 export default NameList
