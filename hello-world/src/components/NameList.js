30bd793047528371e68daea0e69ef4564932d93f
Gino <gino.jameskutty@gmail.com>
2020-11-21 22:00:22 +0530

    List rendering: array of person properties

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -1,11 +1,34 @@
 import React from 'react'
 
 function NameList() {
- const names=['Bruce','Clark','Batman']
- const nameList =names.map(name => <h2>{name}</h2>)
+    const persons=[
+        {
+            id:1,
+            name:'Bruce',
+            age: 30,
+            skill:'React'
+        },
+{
+            id:2,
+            name:'Wayne',
+            age: 40,
+            skill:'Java'
+       },  {
+            id:3,
+            name:'Thomas',
+            age: 33,
+            skill:'C#'
+       }, {
+            id:4,
+            name:'Batman',
+            age: 90,
+            skill:'Python'
+       } 
+    ]
+ const personList =persons.map(person => <h2>Name: {person.name}, Age: {person.age}, Skill: {person.skill}</h2>)
     return (
         <div>
-            {nameList}
+            {personList}
         </div>
     )
 }
