7f9b484308eb0123ec5fe5c99e73c3fbafe86cbc
Gino <gino.jameskutty@gmail.com>
2020-11-21 22:38:54 +0530

    List rendering: Two children with same key error

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -2,34 +2,11 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
-    const persons=[
-        {
-            id:1,
-            name:'Bruce',
-            age: 30,
-            skill:'React'
-        },
-{
-            id:2,
-            name:'Wayne',
-            age: 40,
-            skill:'Java'
-       },  {
-            id:3,
-            name:'Thomas',
-            age: 33,
-            skill:'C#'
-       }, {
-            id:4,
-            name:'Batman',
-            age: 90,
-            skill:'Python'
-       } 
-    ]
- const personList =persons.map(person => <Person key={person.id} person={person}></Person>)
+    const names= ['Bruce','Clark','Diana', 'Bruce']
+    const nameList = names.map(name=><h2 key={name}>{name}</h2>)
     return (
         <div>
-            {personList}
+            {nameList}
         </div>
     )
 }
--- hello-world/src/components/Person.js
+++ hello-world/src/components/Person.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Person({person}) {
     return (
         <div>
-     <h2>Name: {person.name}, Age: {person.age}, Skill: {person.skill}</h2>       
+     <h2>Key:{person.id}, Name: {person.name}, Age: {person.age}, Skill: {person.skill}</h2>       
         </div>
     )
 }
