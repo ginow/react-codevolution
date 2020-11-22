c0eac96adde670fb7c7d388f7cfc21e066acd52c
Gino <gino.jameskutty@gmail.com>
2020-11-22 17:06:24 +0530

    React.Fragmentfor list

--- hello-world/src/components/Columns.js
+++ hello-world/src/components/Columns.js
@@ -1,11 +1,20 @@
 import React from 'react'
 
 function Columns() {
+    const items = [{ title: 'firstlistitem' }, { title: 'secondlistitem' }]
     return (
         <React.Fragment>
+            {
+                items.map(item => (
+                    < React.Fragment key={item.id}>
+                        <h1>Title</h1>
+                        <p>{item.title}</p>
+                    </React.Fragment>
+                ))
+            }
             <td>Name</td>
             <td>Vishwas</td>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
 
