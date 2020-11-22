b379624b7ae54af1fa17bff92dc49824c3a9856b
Gino <gino.jameskutty@gmail.com>
2020-11-22 11:42:52 +0530

    Component lifecyle in update phase

--- hello-world/src/components/LIfecylceB.js
+++ hello-world/src/components/LIfecylceB.js
@@ -18,6 +18,18 @@ class LifecycleB extends Component {
     componentDidMount(){
         console.log('LifecycleB componentDidMount')
     }
+    shouldComponentUpdate(){
+        console.log('LifecycleB shouldComponentUpdate')
+        return true
+    }
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log('LifecycleB getSnapshotBeforeUpdate')
+        return null
+    }
+    componentDidUpdate(){
+        console.log('LifecycleB componentDidUpdate')
+    }
+
     render() {
         console.log('LifecycleB render')
         return (
--- hello-world/src/components/LifecycleA.js
+++ hello-world/src/components/LifecycleA.js
@@ -17,11 +17,28 @@ class LifecycleA extends Component {
     componentDidMount(){
         console.log('LifecycleA componentDidMount')
     }
+    shouldComponentUpdate(){
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+    getSnapshotBeforeUpdate(prevProps,prevState){
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+    componentDidUpdate(){
+        console.log('LifecycleA componentDidUpdate')
+    }
+    changeState=()=>{
+        this.setState({
+            name:'Codevolution'
+        })
+    }
     render() {
         console.log('LifecycleA render')
         return (
             <div>
                LifecycleA 
+               <button onClick={this.changeState}>Change state</button>
                <LifecycleB></LifecycleB>
             </div>
         )
