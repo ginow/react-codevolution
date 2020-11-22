b18019212c61e0822324e3e4a8eece012bbde6c4
Gino <gino.jameskutty@gmail.com>
2020-11-22 22:06:53 +0530

    Refs: Callbacktechnique - Focus on text input

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -4,16 +4,27 @@ class RefsDemo extends Component {
     constructor(props) {
         super(props)
         this.inputRef = React.createRef()
+        this.cbRef = null
+        this.setCbRef = (element) => {
+            this.cbRef = element
+        }
     }
     componentDidMount() {
-        this.inputRef.current.focus()
-        console.log(this.inputRef)
+        // Directly access the DOM without using .current 
+        if (this.cbRef) {
+            this.cbRef.focus()
+        }
+    }
+    clickHandler = () => {
+        alert(this.inputRef.current.value)
     }
 
     render() {
         return (
             <div>
-                <input type="text" ref={this.inputRef}></input>
+                <input type="text" ref={this.inputRef} placeholder='Input Ref'></input>
+                <input type="text" ref={this.setCbRef} placeholder='Call Back ref'></input>
+                <button onClick={this.clickHandler}>Click</button>
             </div>
         )
     }
