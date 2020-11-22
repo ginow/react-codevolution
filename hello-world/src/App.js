efea91ded8f69b46b321a466f3134d859b7aecee
Gino <gino.jameskutty@gmail.com>
2020-11-22 16:41:09 +0530

    React.Fragment instead of div

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import LifecycleA from './components/LifecycleA'
+import FragmentDemo from './components/FragmentDemo';
 function App() {
   return (
     <div className="App">
-      <LifecycleA></LifecycleA>
+      <FragmentDemo></FragmentDemo>
     </div>
   );
 }
---
+++ hello-world/src/components/FragmentDemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FragmentDemo() {
+    return (
+        <React.Fragment>
+        <h1>
+    Framgment Demo        
+        </h1>
+        <p>this describes fragment demo</p>
+        </React.Fragment>
+    )
+}
+
+export default FragmentDemo
