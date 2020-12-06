2521d820387d83e7ab3a22d39618705b90ac2c4a
Gino <gino.jameskutty@gmail.com>
2020-12-06 22:40:12 +0530

    Post request

--- react-http/src/App.js
+++ react-http/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 function App() {
   return (
     <div className="App">
-      <PostList></PostList>
+      <PostForm></PostForm>
     </div>
   );
 }
