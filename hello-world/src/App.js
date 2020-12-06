67f3b90b2f1ba154a052516a79689558ec4aaf22
Gino <gino.jameskutty@gmail.com>
2020-12-06 19:31:53 +0530

    Default Context value

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { UserProvider } from './components/userContext';
 import ComponentC from './components/ComponentC';
+import ComponentF from './components/ComponentF';
 
 function App() {
   return (
     <div className="App">
-      <UserProvider value="ContextPassingValueText"><ComponentC></ComponentC></UserProvider>
+      <ComponentC>
+      </ComponentC>
     </div>
   );
 }
--- hello-world/src/components/userContext.js
+++ hello-world/src/components/userContext.js
@@ -1,5 +1,5 @@
 import React from 'react'
-const UserContext = React.createContext()
+const UserContext = React.createContext('defaultValueText')
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 export { UserProvider, UserContext, UserConsumer }
