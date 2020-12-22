7c8d5487f8a0afa6bd584b79bb088fd8bae34360
Gino <gino.jameskutty@gmail.com>
2020-12-23 00:12:49 +0530

    Incorrect code to increment by 5

--- state-hook/src/components/HookCounterTwo.js
+++ state-hook/src/components/HookCounterTwo.js
@@ -3,12 +3,18 @@ import React, { useState } from 'react'
 function HookCounterTwo() {
     const initialCount = 0
     const [count, setCount] = useState(initialCount)
+    const incrementFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(count + 1)
+        }
+    }
     return (
         <div>
             Count:{count}
             <button onClick={() => setCount(initialCount)}>Reset</button>
             <button onClick={() => setCount(count + 1)}>Increment </button>
             <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={incrementFive}>Increment 5</button>
         </div>
     )
 }
