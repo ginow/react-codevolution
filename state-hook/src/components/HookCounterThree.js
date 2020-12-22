cf556c0372212b2680cb76ab674168362a99ace6
Gino <gino.jameskutty@gmail.com>
2020-12-23 00:35:31 +0530

    correct code by using spread operator

--- state-hook/src/components/HookCounterThree.js
+++ state-hook/src/components/HookCounterThree.js
@@ -6,11 +6,11 @@ function HookCounterThree() {
         <form>
             <input type="text"
                 value={name.firstName}
-                onChange={e => setName({ firstName: e.target.value })}
+                onChange={e => setName({ ...name, firstName: e.target.value })}
             />
             <input type="text"
                 value={name.lastName}
-                onChange={e => setName({ lastName: e.target.value })}
+                onChange={e => setName({ ...name, lastName: e.target.value })}
             />
             <h2>Your first name is - {name.firstName}</h2>
             <h2>Your last name is - {name.lastName}</h2>
