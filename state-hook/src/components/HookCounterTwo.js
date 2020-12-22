ad2499600c655057e41c0a80f1d365bda5ce05db
Gino <gino.jameskutty@gmail.com>
2020-12-23 00:16:48 +0530

    Correct code to increment and decrement

--- state-hook/src/components/HookCounterTwo.js
+++ state-hook/src/components/HookCounterTwo.js
@@ -5,15 +5,15 @@ function HookCounterTwo() {
     const [count, setCount] = useState(initialCount)
     const incrementFive = () => {
         for (let i = 0; i < 5; i++) {
-            setCount(count + 1)
+            setCount(prevCount => prevCount + 1)
         }
     }
     return (
         <div>
             Count:{count}
             <button onClick={() => setCount(initialCount)}>Reset</button>
-            <button onClick={() => setCount(count + 1)}>Increment </button>
-            <button onClick={() => setCount(count - 1)}>Decrement</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Increment </button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button>
             <button onClick={incrementFive}>Increment 5</button>
         </div>
     )
