ebca1d49e96299c273b205be640677f16e56d670
Gino <gino.jameskutty@gmail.com>
2020-12-23 13:55:24 +0530

    Declaring function in useEffect to remember dependency

--- state-hook/src/components/IntervalHookCounter.js
+++ state-hook/src/components/IntervalHookCounter.js
@@ -5,12 +5,18 @@ function IntervalHookCounter() {
     const tick = () => {
         setCount(prevCount => prevCount + 1)
     }
+
     useEffect(() => {
+        // Declare function here itself to remind that the useEffect is dependent on a prop
+        function doSomething() {
+            console.log(someProp)
+        }
+        doSomething()
         const interval = setInterval(tick, 1000)
         return () => {
             clearInterval(interval)
         }
-    }, [])
+    }, [someProp])
     return (
         <div>
             {count}
