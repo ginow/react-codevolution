d5cc73da1ed285ffdec89550be5eb8870d82f4ad
Gino <gino.jameskutty@gmail.com>
2020-12-23 13:52:16 +0530

    Without dependency array, using prevCount

--- state-hook/src/components/IntervalHookCounter.js
+++ state-hook/src/components/IntervalHookCounter.js
@@ -3,14 +3,14 @@ import React, { useState, useEffect } from 'react'
 function IntervalHookCounter() {
     const [count, setCount] = useState(0)
     const tick = () => {
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
     useEffect(() => {
         const interval = setInterval(tick, 1000)
         return () => {
             clearInterval(interval)
         }
-    }, [count])
+    }, [])
     return (
         <div>
             {count}
