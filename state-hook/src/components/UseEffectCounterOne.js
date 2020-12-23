86cc2d9047d080c8aa0182dccf183a09a6e072f4
Gino <gino.jameskutty@gmail.com>
2020-12-23 08:57:44 +0530

    Unoptimised use of useEffect

--- state-hook/src/components/UseEffectCounterOne.js
+++ state-hook/src/components/UseEffectCounterOne.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 function UseEffectCounterOne() {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState('')
     useEffect(() => {
+        console.log('useEffect - Updating document title')
         document.title = `You clicked ${count} times`
     })
     return (
         <div>
+            <input type='text' value={name} onChange={e => setName(e.target.value)} />
             <button onClick={() => setCount(count + 1)}> Click {count} times</button>
         </div>
     )
