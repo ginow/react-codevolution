6efb7290e827da0582b6fb205f9f4b983064c87a
Gino <gino.jameskutty@gmail.com>
2020-12-23 12:57:48 +0530

    UseEffect with incorrect dependency

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import HookMouse from './components/HookMouse';
 import IntervalClassCounter from './components/IntervalClassCounter';
+import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
 function App() {
   return (
     <div className="App">
-      <IntervalClassCounter></IntervalClassCounter>
+      <IntervalHookCounter></IntervalHookCounter>
     </div>
   );
 }
