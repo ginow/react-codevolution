94cecbbf36c7743b2dbb4ff2468604a6640335c1
Gino <gino.jameskutty@gmail.com>
2020-12-23 20:37:45 +0530

    With useContext Hook multi (nested) context implementation

--- state-hook/src/components/ComponentE.js
+++ state-hook/src/components/ComponentE.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ComponentF from './ComponentF'
+import { UserContext, ChannelContext } from '../App'
 
 function ComponentE() {
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
     return (
         <div>
-            <ComponentF></ComponentF>
+            {user}-{channel}
         </div>
     )
 }
