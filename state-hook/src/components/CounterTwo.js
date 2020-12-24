aa8d9b1e292a17eaa076c134350f8927e3b1a45a
Gino <gino.jameskutty@gmail.com>
2020-12-24 20:37:36 +0530

    useReducer using state as an object

--- state-hook/src/components/CounterTwo.js
+++ state-hook/src/components/CounterTwo.js
@@ -2,14 +2,20 @@
 import React, { useReducer } from 'react'
 
 const initialState = {
-    firstCounter: 0
+    firstCounter: 0,
+    secondCounter: 10
 }
 const reducer = (state, action) => {
     switch (action.type) {
+        // Keep the other state values untouched, so use spread operator
         case 'increment':
-            return { firstCounter: state.firstCounter + action.value }
+            return { ...state, firstCounter: state.firstCounter + action.value }
         case 'decrement':
-            return { firstCounter: state.firstCounter - action.value }
+            return { ...state, firstCounter: state.firstCounter - action.value }
+        case 'increment2':
+            return { ...state, secondCounter: state.secondCounter + action.value }
+        case 'decrement2':
+            return { ...state, secondCounter: state.secondCounter - action.value }
         case 'reset':
             return initialState
         default:
@@ -22,8 +28,11 @@ function CounterTwo() {
     return (
         <div>
             <div>Count - {count.firstCounter}</div>
+            <div>second Count - {count.secondCounter}</div>
             <button onClick={() => dispatch({ type: 'increment', value: 1 })}>Increment</button>
             <button onClick={() => dispatch({ type: 'decrement', value: 1 })}>Decrement</button>
+            <button onClick={() => dispatch({ type: 'increment2', value: 1 })}>Increment 2nd counter</button>
+            <button onClick={() => dispatch({ type: 'decrement2', value: 1 })}>Decrement 2nd counter</button>
             <button onClick={() => dispatch({ type: 'increment', value: 5 })}>Increment by 5</button>
             <button onClick={() => dispatch({ type: 'decrement', value: 5 })}>Decrement by 5</button>
             <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
