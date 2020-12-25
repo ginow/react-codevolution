24d440cade80fb474dbef04b9bfea251eb07eefa
Gino <gino.jameskutty@gmail.com>
2020-12-25 13:44:41 +0530

    Fetching data with useReducer

--- state-hook/src/App.js
+++ state-hook/src/App.js
@@ -1,40 +1,14 @@
 import React, { useReducer } from 'react'
 import './App.css';
-import ComponentA from './components/useReducerWithUseContext/ComponentA'
-import ComponentB from './components/useReducerWithUseContext/ComponentB'
-import ComponentC from './components/useReducerWithUseContext/ComponentC'
+import DataFetchingOne from './components/DataFetchingOne';
 
 export const CountContext = React.createContext()
 
-const initialState = 0
-const reducer = (state, action) => {
-  switch (action) {
-    case 'increment':
-      return state + 1
-    case 'decrement':
-      return state - 1
-    case 'reset':
-      return initialState
-    default:
-      return state
-  }
-}
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
-      <div className="App">
-        Count : {count}
-        {/* Goal is to maintain counter state in App.js and modify it from other components
-          Steps:
-          1. Create counter using useReducer in App.js
-          2. Provide and consume counter context in the required components
-      */}
-        <ComponentA></ComponentA>
-        <ComponentB></ComponentB>
-        <ComponentC></ComponentC>
-      </div>
-    </CountContext.Provider>
+    <div className='App'>
+      <DataFetchingOne></DataFetchingOne>
+    </div>
   );
 }
 
---
+++ state-hook/src/components/DataFetchingOne.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function DataFetchingOne() {
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+    const [post, setPost] = useState({})
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+            .then(response => {
+                setLoading(false)
+                setPost(response.data)
+                setError('')
+            })
+            .catch(error => {
+                setLoading(false)
+                setPost({})
+                setError('Something went wrong!')
+            })
+    }, [])
+    return (
+        <div>
+            {loading ? 'Loading' : post.title}
+            {error ? error : null}
+        </div>
+    )
+}
+
+export default DataFetchingOne
