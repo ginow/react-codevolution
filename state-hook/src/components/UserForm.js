e2576fd615fb8adbd03ff19f65db0d4d10abba7b
Gino <gino.jameskutty@gmail.com>
2021-01-01 18:49:03 +0530

    Custom Hook: Third example with custom hook

--- state-hook/src/components/UserForm.js
+++ state-hook/src/components/UserForm.js
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
+import useInput from './hooks/useInput'
 
 function UserForm() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
+    const [firstName, bindFirstName, resetFirstName] = useInput('')
+    const [lastName, bindLastName, resetLastName] = useInput('')
     const submitHandler = e => {
         e.preventDefault();
         alert(`Hello ${firstName} ${lastName}`)
+        resetFirstName();
+        resetLastName();
     }
     return (
         <div>
             <form onSubmit={submitHandler}>
                 <div>
                     <label>First name</label>
-                    <input value={firstName}
-                        onChange={e => setFirstName(e.target.value)}
+                    <input
+                        {...bindFirstName}
                         type='text' />
                 </div>
                 <div>
                     <label>Last name</label>
-                    <input value={lastName}
-                        onChange={e => setLastName(e.target.value)}
+                    <input
+                        {...bindLastName}
                         type='text' />
                 </div>
                 <button>Submit</button>
