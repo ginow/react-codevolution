95026a9ae809cf1044d722bc249b756efcd4abd5
Gino <gino.jameskutty@gmail.com>
2021-05-13 12:43:55 +0530

    Bug fix - react-router-dom reinstalled

--- react-router/routing/src/App.js
+++ react-router/routing/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import Nav from './Nav';
 import About from './About';
 import Shop from './Shop';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Nav></Nav>
-      <About></About>
-      <Shop></Shop>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav></Nav>
+        <About></About>
+        <Shop></Shop>
+      </div>
+    </Router>
   );
 }
 
