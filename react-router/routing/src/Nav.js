676e8416c97e5ee5fe4b6c07d004fd4a89b53e9e
Gino <gino.jameskutty@gmail.com>
2021-05-13 13:17:04 +0530

    Link in navigation bar

--- react-router/routing/src/Nav.js
+++ react-router/routing/src/Nav.js
@@ -1,13 +1,25 @@
 
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
+    const navStyle = {
+        color: 'white'
+    };
     return (
         <nav>
             <h1>Logo</h1>
             <ul className="nav-links">
-                <li>About</li>
-                <li>Shop</li>
+                {/*href will reload the page so use "Link"*/}
+                <Link style={navStyle} to="/">
+                    <li>Home</li>
+                </Link>
+                <Link style={navStyle} to="/about">
+                    <li>About</li>
+                </Link>
+                <Link style={navStyle} to="/shop">
+                    <li>Shop</li>
+                </Link>
             </ul>
         </nav>
     );
